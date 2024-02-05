58071782b0c696786f6da221a5fd95c794be7ff6
Mattchew Higgins <higgimat000@warren.k12.in.us>
2024-02-05 08:32:02 -0500

    initial commit

---
+++ Unit 04_Objects, Events, Strings, and Numbers/03_String Basics/script.js
@@ -0,0 +1,42 @@
+let lyrics =
+`
+Jesus, I am overjoyed to meet you face to face
+You've been getting quite a name all around the place
+Healing cripples, raising from the dead
+And now, I understand you're God
+At least, that's what you've said
+
+So, you are the Christ, you're the great Jesus Christ
+Prove to me that you're divine - change my water into wine
+That's all you need do, and I'll know it's all true
+C'mon, king of the Jews!
+
+Jesus, you just won't believe the hit you've made round here
+You are all we talk about, the wonder of the year!
+Oh, what a pity if it's all a lie
+Still, I'm sure that you can rock the cynics if you try
+
+So, you are the Christ, you're the great Jesus Christ
+Prove to me that you're no fool - walk across my swimming pool
+If you do that for me, then I'll let you go free
+C'mon, king of the Jews!
+
+I only ask things I'd ask any superstar
+What is it that you have got that puts you where you are?
+I am waiting, yes, I'm a captive fan
+I'm dying to be shown that you are not just any man
+
+So, if you are the Christ, yes, the great Jesus Christ
+Feed my household with this bread - you can do it on your head!
+Or has something gone wrong? Why do you take so long?
+C'mon, king of the Jews!
+
+Hey! Aren't you scared of me Christ? Mr Wonderful Christ!
+You're a joke, you're not the Lord
+You are nothing but a fraud
+Take him away - he's got nothing to say!
+
+Get out, you king of the Jews! Get out of my life!
+`;
+
+document.getElementById("lyrics").innerHTML = lyrics
