0e0d6c6f977cd09e62d133d1a1af6a2338d0e956
Mattchew Higgins <higgimat000@warren.k12.in.us>
2024-02-08 09:09:29 -0500

    initial commit

--- Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script2.js
+++ Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script2.js
@@ -6,3 +6,16 @@ function takeSquareRoot() {
     document.getElementById("result").innerHTML = squareRoot;
     
 }
+
+
+function takeAbs() {
+    let number = +document.getElementById("input1").value;
+    number = Math.abs(number);
+    document.getElementById("abs").innerHTML = number;
+}
+
+function takeCosine() {
+    let number = +document.getElementById("input1").value;
+    number = Math.cos(number)
+    document.getElementById("cos").innerHTML = number;
+}
