7d484e36abd40d6e773e65863f4a516a4100e1af
Mattchew Higgins <higgimat000@warren.k12.in.us>
2024-02-29 08:51:28 -0500

    initial commit

--- Unit 08_Data Structures/01_Arrays/script.js
+++ Unit 08_Data Structures/01_Arrays/script.js
@@ -1,8 +1,16 @@
 const numbers = []
 
+//UNSORTED
+
 for (let i = 0; i < 10; i++) {
     let num = Math.floor(Math.random() * 10) + 1;
     numbers.push(num);
 }
 
 document.getElementById("unsorted").innerHTML = numbers;
+
+//SORTED
+
+numbers.sort(function(a, b){return a - b});
+
+document.getElementById("sorted").innerHTML = numbers;
