6470202afd465d38d72fa545336829881c3d5ad4
Mattchew Higgins <higgimat000@warren.k12.in.us>
2024-03-04 08:54:10 -0500

    initial commit

---
+++ Unit 08_Data Structures/02_Arrays again/script.js
@@ -0,0 +1,34 @@
+// const numbers = []
+
+// //UNSORTED
+
+// for (let i = 0; i < 10; i++) {
+//     let num = Math.floor(Math.random() * 10) + 1;
+//     if(num % 2 !== 0){
+//         numbers.push(num);
+//     }
+// }
+
+// document.getElementById("unsorted").innerHTML = numbers;
+
+// //SORTED
+
+// numbers.sort(function(a, b){return a - b});
+
+// document.getElementById("sorted").innerHTML = numbers;
+
+
+const numbers = []
+
+for (let i = 0; i < 10; i++) {
+    let num = Math.floor(Math.random() * 10) + 1;
+    numbers.push(num);
+}
+
+function filterOutEvens(number){
+    return number % 2 === 1
+}
+
+let filteredNumbers = numbers.filter(filterOutEvens);
+
+document.getElementById("unsorted").innerHTML = filteredNumbers;
