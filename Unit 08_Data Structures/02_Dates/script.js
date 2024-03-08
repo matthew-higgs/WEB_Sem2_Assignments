c11949af393edfb97c9d84a608abb26f6d028f08
Mattchew Higgins <higgimat000@warren.k12.in.us>
2024-03-08 08:44:38 -0500

    initial commit

--- Unit 08_Data Structures/02_Dates/script.js
+++ Unit 08_Data Structures/02_Dates/script.js
@@ -1,48 +1,49 @@
 const currentDate = new Date();
 const month = currentDate.getMonth();
 const year = currentDate.getFullYear();
-switch (month) {
-    case 0:
-        document.getElementById("month").innerHTML = "January";
-        break;
-    case 1:
-        document.getElementById("month").innerHTML = "February";
-        break;
-    case 2:
-        document.getElementById("month").innerHTML = "March";
-        break;
-    case 3:
-        document.getElementById("month").innerHTML = "April";
-        break;
-    case 4:
-        document.getElementById("month").innerHTML = "May";
-        break;
-    case 5:
-        document.getElementById("month").innerHTML = "June";
-        break;
-    case 6:
-        document.getElementById("month").innerHTML = "July";
-        break;
-    case 7:
-        document.getElementById("month").innerHTML = "August";
-        break;
-    case 8:
-        document.getElementById("month").innerHTML = "September";
-        break;
-    case 9:
-        document.getElementById("month").innerHTML = "October";
-        break;
-    case 10:
-        document.getElementById("month").innerHTML = "November";
-        break;
-    case 11:
-        document.getElementById("month").innerHTML = "December";
-        break;
-    default:
-        document.getElementById("month").innerHTML = "Unknown Month";
-        break;
+function generateCalendar(year, month) {
+    switch (month) {
+        case 0:
+            document.getElementById("month").innerHTML = "January " + year;
+            break;
+        case 1:
+            document.getElementById("month").innerHTML = "February " + year;
+            break;
+        case 2:
+            document.getElementById("month").innerHTML = "March " + year;
+            break;
+        case 3:
+            document.getElementById("month").innerHTML = "April " + year;
+            break;
+        case 4:
+            document.getElementById("month").innerHTML = "May " + year;
+            break;
+        case 5:
+            document.getElementById("month").innerHTML = "June " + year;
+            break;
+        case 6:
+            document.getElementById("month").innerHTML = "July " + year;
+            break;
+        case 7:
+            document.getElementById("month").innerHTML = "August " + year;
+            break;
+        case 8:
+            document.getElementById("month").innerHTML = "September " + year;
+            break;
+        case 9:
+            document.getElementById("month").innerHTML = "October " + year;
+            break;
+        case 10:
+            document.getElementById("month").innerHTML = "November " + year;
+            break;
+        case 11:
+            document.getElementById("month").innerHTML = "December " + year;
+            break;
+        default:
+            document.getElementById("month").innerHTML = "Unknown Month";
+            break;
+    }
 }
-
 document.getElementById("month").innerHTML = document.getElementById("month").innerHTML +  " " + year;
 
 
@@ -86,22 +87,21 @@ const calendarMap = new Map([
 
 const firstDayDate = new Date(year, month, 1);
 const firstDayNumber = firstDayDate.getDay() + 1;
+
 const lastDayofMonth = new Date(year, month + 1, 1);
 lastDayofMonth.setDate(lastDayofMonth.getDate() - 1);
+
 const lastDayOfMonthNumber = lastDayofMonth.getDate();
 console.log(lastDayOfMonthNumber);
 
 for(let i = 1; i <= 35; i++){
-    let check = i - firstDayDate + 1;
+    let dayNum = i - firstDayNumber + 1;
+    let id = calendarMap.get(i);
 
-    if(i < firstDayNumber){
-        let id = calendarMap.get(i);
-        document.getElementById(id).innerHTML = "";
+    if (dayNum >= 1 && dayNum <= 31) {
+        document.getElementById(id).innerHTML = dayNum;
     }
-
-    else if(check >= 1 && check <= lastDayOfMonthNumber){
-        let id = calendarMap.get(i);
-        console.log(id)
-        document.getElementById(id).innerHTML = check;
+    else {
+        document.getElementById(id).innerHTML = "";
     }
 }
